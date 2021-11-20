86236f86d327a1640d04e7b1f912f9166125e16e
kchatr <kchatr1729@gmail.com>
2021-11-19 19:36:25 -0500

    Added Colour + OnTap Function

--- lib/game/squares.dart
+++ lib/game/squares.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe/game/player.dart';
 
 class Square extends StatefulWidget {
   const Square({Key? key}) : super(key: key);
@@ -8,15 +9,36 @@ class Square extends StatefulWidget {
 }
 
 class _SquaresState extends State<Square> {
+  String symbol = "-";
+  bool playable = true;
+  Color? gridCol = Colors.grey[200];
+
+  void play() {
+    setState(() {
+      symbol = Player.getPlayer() == 1 ? "X" : "O";
+      playable = false;
+      gridCol = Player.getPlayer() == 1 ? Colors.red[200] : Colors.blue[200];
+    });
+    Player.updatePlayer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      width: 100,
+      color: gridCol,
       child: ButtonTheme(
+        hoverColor: Colors.pink,
         child: OutlinedButton(
-          onPressed: () {},
-          child: Text("test"),
+          onPressed: () {
+            playable ? play() : null;
+          },
+          child: Text(
+            symbol,
+            style: TextStyle(
+              color:
+                  Player.getPlayer() == 1 ? Colors.red[100] : Colors.blue[100],
+            ),
+          ),
         ),
       ),
     );
