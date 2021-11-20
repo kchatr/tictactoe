6d90b4d98af67a5ac0bc31456d4cbd5c938467fc
kchatr <kchatr1729@gmail.com>
2021-11-20 12:41:03 -0500

    Game Stops After Win

--- lib/game/squares.dart
+++ lib/game/squares.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tictactoe/game/player.dart';
+import 'package:tictactoe/game/game.dart';
 
 class Square extends StatefulWidget {
   final int? pos;
@@ -11,9 +12,15 @@ class Square extends StatefulWidget {
 
 class _SquaresState extends State<Square> {
   String symbol = "";
-  bool playable = true;
+  bool playable = Game.getPlayable();
   Color? gridCol = Colors.black87;
   Color? hovCol = Colors.amber[200];
+
+  void gamePlay() {
+    if (Game.winner() != 0) {
+      Game.setPlayable(false);
+    }
+  }
 
   void play() {
     setState(() {
@@ -22,6 +29,8 @@ class _SquaresState extends State<Square> {
       gridCol = Player.getPlayer() == 1 ? Colors.red[200] : Colors.blue[200];
       hovCol = Player.getPlayer() == 1 ? Colors.red[100] : Colors.blue[100];
     });
+    Game.updateBoard(widget.pos!, Player.getPlayer());
+    gamePlay();
     Player.updatePlayer();
   }
 
@@ -33,10 +42,17 @@ class _SquaresState extends State<Square> {
         onTap: () {
           playable ? play() : null;
         },
-        hoverColor: hovCol,
+        onHover: (bool i) {
+          if (i && playable) {
+            setState(() {
+              playable = Game.getPlayable();
+            });
+          }
+        },
+        hoverColor: playable || symbol != "" ? hovCol : Colors.transparent,
         child: Container(
           decoration: BoxDecoration(
-              border: Border.all(color: Colors.grey.shade400, width: 0.2)),
+              border: Border.all(color: Colors.grey.shade400, width: 0.3)),
           child: Center(
             child: Text(
               symbol,
