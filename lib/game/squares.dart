01cb7fc240a801e98f61a379c71df18c7d9af74a
kchatr <kchatr1729@gmail.com>
2021-11-19 19:56:37 -0500

    Refactored Square Widget as InkWell on Container

--- lib/game/squares.dart
+++ lib/game/squares.dart
@@ -9,34 +9,42 @@ class Square extends StatefulWidget {
 }
 
 class _SquaresState extends State<Square> {
-  String symbol = "-";
+  String symbol = "";
   bool playable = true;
-  Color? gridCol = Colors.grey[200];
+  Color? gridCol = Colors.grey[50];
+  Color? hovCol = Colors.red[50];
 
   void play() {
     setState(() {
       symbol = Player.getPlayer() == 1 ? "X" : "O";
       playable = false;
       gridCol = Player.getPlayer() == 1 ? Colors.red[200] : Colors.blue[200];
+      hovCol = Player.getPlayer() == 1 ? Colors.red[50] : Colors.blue[50];
     });
     Player.updatePlayer();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Material(
       color: gridCol,
-      child: ButtonTheme(
-        hoverColor: Colors.pink,
-        child: OutlinedButton(
-          onPressed: () {
-            playable ? play() : null;
-          },
-          child: Text(
-            symbol,
-            style: TextStyle(
-              color:
-                  Player.getPlayer() == 1 ? Colors.red[100] : Colors.blue[100],
+      child: InkWell(
+        onTap: () {
+          playable ? play() : null;
+        },
+        hoverColor: hovCol,
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey.shade700, width: 0.2)),
+          child: Center(
+            child: Text(
+              symbol,
+              style: TextStyle(
+                color: Player.getPlayer() == 1
+                    ? Colors.red[100]
+                    : Colors.blue[100],
+                fontSize: 40,
+              ),
             ),
           ),
         ),
